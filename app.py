5248a408af8df3064e4f1ad9a61b884be189402f
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-09-08 12:29:53 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1398,7 +1398,12 @@ else:
     # 🔹 Sub-abas principais (só aparecem quando o caixa está aberto)
         tab1, tab2, tab3 = st.tabs(["Venda Detalhada", "Últimas Vendas", "Recibos de Vendas"])
 
-        # ================= TAB 1 - VENDA DETALHADA =================
+        
+
+
+
+
+       # ================= TAB 1 - VENDA DETALHADA =================
         with tab1:
             st.subheader("🛒 Venda Detalhada")
 
